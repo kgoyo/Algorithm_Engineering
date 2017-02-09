c45195ae714e5f96bedaef544a47b2a1bc1020af
Andreas Østergaard Nielsen <kgoyo666@gmail.com>
2017-02-09 13:24:04 +0100

    sorted binary search implemented

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -24,15 +24,51 @@ int * loadArrayfromFile(const char filePath[], int& LENGTH) {
 	return array;
 }
 
+
+void binarySearchSorted(int array[], int LENGTH, int numRuns) {
+	srand((unsigned)time(NULL)); //init seed
+
+	for (int i = 0; i < numRuns; i++) {
+		int key = rand() % array[LENGTH - 1];
+		cout << "searching with key: " << key << endl;
+
+		int low = 0, high = LENGTH - 1, midpoint;
+
+		while (low <= high) {
+			midpoint = low + (high - low) / 2;
+			if (key == array[midpoint]) {
+				//if found
+				cout << "found " << array[midpoint] << endl;
+				break;
+			}
+			else if (key < array[midpoint]) {
+				high = midpoint - 1;
+			}
+			else {
+				low = midpoint + 1;
+			}
+		}
+		int last = array[midpoint];
+		if (last > key) {
+			last = array[midpoint - 1];
+		}
+
+		cout << "found approx " << last << endl;
+	}
+}
+
 int main(int argc, const char* argv[]) {
 	int *array;
 	int LENGTH;
 	array = loadArrayfromFile("randomSortedArray.txt", LENGTH);
+	/*
 	for (int i = 0; i < LENGTH; i++) {
 		cout << array[i] << endl;
 	}
+	*/
+
+	binarySearchSorted(array, LENGTH, 50);
 
 	//delete array
 	delete[] array;
 }
-
